e3125cbdccd4c47e6ee1b1f59eea26a5fdafbe83
shaochangming <014037@springtour.com>
2026-08-31 17:51:24 +0800

    优化打包

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,19 @@ export default defineConfig({
   publicDir: 'public',
   build: {
     outDir: 'dist',
-    sourcemap: true,
+    sourcemap: false,
+    rolldownOptions: {
+      output: {
+        // Vendor 单独成 chunk：业务代码迭代时 three/cannon 缓存不失效，
+        // 回访用户每次更新只需重新下载 ~17KB gzip 的业务 chunk
+        advancedChunks: {
+          groups: [
+            { name: 'three', test: /node_modules[\\/]three[\\/]/ },
+            { name: 'cannon', test: /node_modules[\\/]cannon-es[\\/]/ },
+          ],
+        },
+      },
+    },
   },
   server: {
     port: 3000,
